16b3ff64c3ce0706e3a5b5d98c294f54beee86cc
Arjun Sekar <116697912+carbonvibes@users.noreply.github.com>
2023-09-19 22:25:49 +0530

    Update a1.c

--- a1.c
+++ a1.c
@@ -1,5 +1,14 @@
-#include "test.h"
+//#include "test.h"
+#include<stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+void process_selection(int a[],int k,int ind[]){
+    for(int i=0;i<k;i++){
+        printf("%d ",a[ind[i]]);
+    }
+    printf("\n");
+}
 /*
  * Generate k-selections of a[0..n-1] in lexicographic order and call process_selection to process them.
  *
@@ -8,8 +17,19 @@
  * Selections should be generated in lexicographic order.
  * a[0..k-1] is the smallest selection and a[n-k..n-1] is the largest.
  */
-void generate_selections(int a[], int n, int k, int b[], void *data, void (*process_selection)(int *b, int k, void *data))
-{}
+void generate_selections(int a[],int k,int ind[],int start,int ci,int n)
+{
+    if(ci==k){
+        process_selection(a,k,ind);
+        return;
+    }
+        
+    for(int i=start;i<n;i++){
+        ind[ci]=i;
+        generate_selections(a,k,ind,i+1,ci+1,n);
+    }
+    
+}
 
 /*
  * See Exercise 2 (a), page 94 in Jeff Erickson's textbook.
@@ -31,7 +51,12 @@ void previous_permutation(int a[], int n)
 
 /* Write your tests here. Use the previous assignment for reference. */
 
-int main()
-{
-    return 0;
+int main(){
+    int a[] ={2,3,1,5,6};
+    int k=3;
+    int ind[k];
+    int ci=0;
+    int n = sizeof(a) / sizeof(a[0]);
+    generate_selections(a,k,ind,0,0,n);
+
 }

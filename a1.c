9a08e7b232d8e343e2d26d1be6990f907c7de868
vamsi2702 <117572851+vamsi2702@users.noreply.github.com>
2023-09-21 14:32:28 +0530

    Solved probelm2

--- a1.c
+++ a1.c
@@ -39,8 +39,53 @@ void generate_selections(int a[],int k,int ind[],int start,int ci,int n)
  * The dictionary parameter is an array of words, sorted in dictionary order.
  * nwords is the number of words in this dictionary.
  */
-void generate_splits(const char *source, const char *dictionary[], int nwords, char buf[], void *data, void (*process_split(char buf[], void *data)))
-{}
+void generate_splits_helper(const char *source, const char *dictionary[], int nwords, char buf[], int buf_index, int source_index, void *data, void (*process_split)(char buf[], void *data)) {
+    int n = strlen(source);
+    // base case
+    if (source_index == n) {
+        if (buf[buf_index]==' '){
+            buf[buf_index-1] = '\0';
+        }
+        else buf[buf_index] = '\0';
+        process_split(buf, data);
+        return;
+    }
+    for (int i = source_index; i < n; i++) {
+
+        int word_length = i - source_index + 1;
+        char word[word_length + 1];
+        strncpy(word, source + source_index, word_length);
+        word[word_length] = '\0';
+        
+        // check if the word is in dictionary
+        int is_word = 0;
+        for (int j = 0; j < nwords; j++) {
+            if (strcmp(word, dictionary[j]) == 0) {
+                is_word = 1;
+                break;
+            }
+        }
+
+        if (is_word) {
+            strcpy(buf + buf_index, word);
+            buf_index += word_length;
+            if (buf_index > 0) {
+                buf[buf_index] = ' ';
+                buf_index++;
+            }
+            // if the word of s from source_index till index i is a valid word call the function from source_index i+1
+            // recurrsive step
+            generate_splits_helper(source, dictionary, nwords, buf, buf_index, i + 1, data, process_split);
+            // to backtrack remove it from buf
+            buf_index -= (word_length + 1);
+        }
+    }
+}
+
+void generate_splits(const char *source, const char *dictionary[], int nwords, char buf[], void *data, void (*process_split)(char buf[], void *data)) {
+    // use helper function
+    generate_splits_helper(source, dictionary, nwords, buf, 0, 0, data, process_split);
+}
 
 /*
  * Transform a[] so that it becomes the previous permutation of the elements in it.
